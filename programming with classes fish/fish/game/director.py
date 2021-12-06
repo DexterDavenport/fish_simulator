0f6eee19f18ec1c0d616b9169f56c4e67c86efdc
DexterDavenport <91489096+DexterDavenport@users.noreply.github.com>
2021-12-06 14:23:58 -0700

    finished code

---
+++ programming with classes fish/fish/game/director.py
@@ -0,0 +1,43 @@
+import os
+os.environ['RAYLIB_BIN_PATH'] = '.'
+
+from game.audio_service import AudioService
+import raylibpy
+from game import constants
+
+audio_service = AudioService()
+
+class Director:
+
+    def __init__(self, cast, script):
+        self._cast = cast
+        self._script = script
+        self._keep_playing = True
+        
+    def start_game(self, cast):
+        while self._keep_playing:
+            self._cue_action("input")
+            self._cue_action("update")
+            self._cue_action("output")
+
+            # TODO: Add some logic like the following to handle game over conditions
+            if len(self._cast["score"]) == 0:
+                audio_service.play_sound(constants.SOUND_OVER)
+                end = cast["end"][0]
+                end_1 =cast["end_1"][0]
+                end.set_image(constants.IMAGE_END_OCEAN)
+                end_1.set_image(constants.IMAGE_DEAD)
+
+            if raylibpy.window_should_close():
+                print('ending 2')
+                self._keep_playing = False
+
+
+    def _cue_action(self, tag):
+        """Executes the actions with the given tag.
+        
+        Args:
+            tag (string): The given tag.
+        """ 
+        for action in self._script[tag]:
+            action.execute(self._cast)
---
+++ programming with classes fish/fish/game/handle_off_screen_action.py
@@ -0,0 +1,47 @@
+from game.point import Point
+from game.action import Action
+from game.audio_service import AudioService
+from game import constants
+import random
+
+audio_service = AudioService()
+
+class Handle_Off_Screen_Action(Action):
+    def __init__(self, physics_service):
+        super().__init__()
+        self._physics_service = physics_service
+
+    def execute(self, cast):
+
+        food = cast["food"][0]
+        fish = cast["fish"][0]
+        scores = cast["score"]
+
+        if food.get_position().get_y() >= 590:
+            x = random.randint(0,790)
+            s = random.randint(2,6)
+            food.set_position(Point(x, 0))
+            food.set_velocity(Point(0, s))
+
+        if fish.get_position().get_x() <= 5:
+            y = fish.get_position().get_y()
+            fish.set_position(Point(6, y))
+
+        if fish.get_position().get_x() >= 685:
+            y = fish.get_position().get_y()
+            fish.set_position(Point(684, y))
+
+        if fish.get_position().get_y() <= 2:
+            x = fish.get_position().get_x()
+            fish.set_position(Point(x, 3))
+
+        if fish.get_position().get_y() >= 551:
+            fish.set_position(Point(345, 40))
+            if len(scores) != 0:
+                audio_service.play_sound(constants.SOUND_BOUNCE)
+                x = len(scores)
+                scores.pop(x-1)
+            if len(scores) == 0:
+                audio_service.play_sound(constants.SOUND_OVER)
+
+
